97a967ea4742250271b47aad3cb304e02c2fb6f9
Michael McCormack <mcctwin@gmail.com>
2025-08-14 20:21:39 -0500

    create login and about pages with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Login from './pages/Login'
 
 const App = () => {
     return (
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
         </BrowserRouter>
     )
